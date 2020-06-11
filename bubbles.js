1fdece73ab993f8d680bd644850e1c68e5fa23f6
neisaah <62030516+neisaah@users.noreply.github.com>
2020-06-10 17:17:42 -0700

    Update bubbles.js

--- bubbles.js
+++ bubbles.js
@@ -1,3 +1,23 @@
+class Bubble {
+  constructor(x, y, r) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+  }
+  
+  move() {
+    this.x = this.x + random(-3, 3);
+    this.y = this.y + random(-3, 3);
+  }
+  
+  show() {
+    stroke(200);
+    strokeWeight(1);
+    fill(15);
+    ellipse(this.x, this.y, this.r*2);
+  }
+}
+
 let bubble1;
 let bubble2;
 let bubble3;
@@ -25,26 +45,4 @@ function draw() {
   bubble4.show();
   bubble5.move();
   bubble5.show();
-}
-
-
-
-class Bubble {
-  constructor(x, y, r) {
-    this.x = x;
-    this.y = y;
-    this.r = r;
-  }
-  
-  move() {
-    this.x = this.x + random(-3, 3);
-    this.y = this.y + random(-3, 3);
-  }
-  
-  show() {
-    stroke(200);
-    strokeWeight(1);
-    fill(15);
-    ellipse(this.x, this.y, this.r*2);
-  }
 }
